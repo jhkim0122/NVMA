4f6efc006959090b6165852c34cdfa0c781c4903
JuHyun Kim <k24504@naver.com>
2022-04-20 17:03:09 +0900

    [chore] Added permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'pages/MainPage.dart';
 
 String appName = "NVMA";
 
-void main() {
+void main() async{
   runApp(const MyApp());
+
+  var status = await Permission.storage.status;
+  if (!status.isGranted) await Permission.storage.request();
+  var manageStatus = await Permission.manageExternalStorage.status;
+  if(!manageStatus.isGranted) await Permission.manageExternalStorage.request();
+  var micStatus = await Permission.microphone.status;
+  if(!micStatus.isGranted) await Permission.microphone.request();
+  var bluetoothStatus = await Permission.bluetoothConnect.status;
+  if(!bluetoothStatus.isGranted) await Permission.bluetoothConnect.request();
+  bluetoothStatus = await Permission.bluetoothScan.status;
+  if(!bluetoothStatus.isGranted) await Permission.bluetoothScan.request();
+  bluetoothStatus = await Permission.bluetoothAdvertise.status;
+  if(!bluetoothStatus.isGranted) await Permission.bluetoothAdvertise.request();
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +30,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Noise Vibration Measurement App',
+      builder: BotToastInit(),
       theme: ThemeData(
         primarySwatch: const MaterialColor(
             0xFFFFC289, <int,Color>{
